1b21f3cf2d319d10059b7a3c6cb703034c955946
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-08-30 17:10:18 +0300

    readded colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -50,6 +50,17 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        gtahidiCream: "#F8EFE4",
+        gtahidiPurple: "#9089B8",
+        gtahidiPurpleTwo: "#e2e7e7",
+        gtahidiPurpleThree: "#EFC3E6",
+        gtahidiPink: "#F577DB",
+        gtahidiBlue: "#1e9bff",
+        gtahidiLightBlue: "#f4faff",
+        gtahidiRed: "#fa8a8c",
+        linearGradientPurple: "#D8DFF1",
+        linearGradientPink: "#F8EFE4",
+        gtahidiDarkBlue: "#494080",
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -73,4 +84,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
