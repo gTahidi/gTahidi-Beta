e5619b24b58737efa33a7e13d0c953620c1b596a
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-08-31 12:41:43 +0300

    Completed main dashboard page

--- components/LeftNav.tsx
+++ components/LeftNav.tsx
@@ -1,7 +1,47 @@
-import { faPlus, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+"use client";
+
+import {
+  faBook,
+  faClipboardQuestion,
+  faComments,
+  faGauge,
+  faPlus,
+  faRightFromBracket,
+  faSpa,
+  faUpload,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
+import { signOut, useSession } from "next-auth/react";
 import React from "react";
+import { DashboardTab } from "./DashboardTab";
+
+export const dashboardTabs = [
+  {
+    icon: faGauge,
+    name: "Dashboard",
+  },
+  {
+    icon: faUpload,
+    name: "Upload Data",
+  },
+  {
+    icon: faSpa,
+    name: "Lesson Plan",
+  },
+  {
+    icon: faBook,
+    name: "Notes",
+  },
+  {
+    icon: faClipboardQuestion,
+    name: "Quizzes",
+  },
+  {
+    icon: faComments,
+    name: "Chat",
+  },
+];
 
 export const LeftNav = ({ className }: { className: string }) => {
   const isBrowser = typeof window !== "undefined"; // added this to Check if running in the browser environment
@@ -11,22 +51,33 @@ export const LeftNav = ({ className }: { className: string }) => {
     isSmallScreen = window.innerWidth < 640;
   }
 
+  const { data: session } = useSession();
+
   return (
     <motion.div
-      className={`bg-black w-1/5 text-white flex flex-col justify-between text-sm px-2 ${className}`}
+      className={`bg-dashboardPurple w-1/5 text-white flex flex-col text-sm px-2 ${className}`}
       initial={isSmallScreen ? { x: -1000 } : undefined}
       animate={isSmallScreen ? { x: 0 } : undefined}
       exit={isSmallScreen ? { x: -1000 } : undefined}
       transition={isSmallScreen ? { duration: 0.5, type: "tween" } : undefined}
     >
-      <div className="flex items-center border-white py-4 px-2 rounded cursor-pointer border-2">
-        <FontAwesomeIcon icon={faPlus} className="w-5 h-5 mr-3" />
-        <p>New Lesson Plan</p>
+      <div className="flex items-center py-4 px-2 rounded cursor-pointer border-2 mt-2">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/gtahidi_logo 2.png" alt="" className="w-5 h-5 mr-3" />
+        <p className="text-center">Gtahidi AI</p>
       </div>
-      <div className="flex items-center border-white py-3 px-2 mb-2 rounded cursor-pointer border-2 w-full sm:w-1/2 ml-auto">
-        <FontAwesomeIcon icon={faRightFromBracket} className="w-5 h-5 mr-3" />
-        <p>Log out</p>
-      </div>
+      {dashboardTabs.map((tab, i) => (
+        <DashboardTab icon={tab.icon} name={tab.name} key={i} />
+      ))}
+      {session?.user && (
+        <div
+          className="flex items-center border-white py-3 px-2 mb-2 rounded cursor-pointer border-2 w-full mt-auto"
+          onClick={() => signOut()}
+        >
+          <FontAwesomeIcon icon={faRightFromBracket} className="w-5 h-5 mr-3" />
+          <p>Log out</p>
+        </div>
+      )}
     </motion.div>
   );
 };
--- components/UserTab.tsx
+++ components/UserTab.tsx
@@ -5,8 +5,8 @@ export const UserTab = () => {
   const { data: session } = useSession();
   if (session && session.user) {
     return (
-      <div className="flex">
-        <div className="text-white text-sm mr-3">
+      <div className="flex ml-auto">
+        <div className="text-black text-sm mr-3">
           <p>{session?.user?.name}</p>
           <p>{session?.user?.email}</p>
         </div>
@@ -15,7 +15,7 @@ export const UserTab = () => {
           <img
             src={session?.user?.image as string}
             alt={session?.user?.name + `s photo`}
-            className="w-10 h-10 outline outline-white outline-2 rounded-full"
+            className="w-10 h-10 outline outline-gtahidiPink outline-2 rounded-full"
           />
         )}
       </div>
