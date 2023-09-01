2e55f36b015843f9b68cbe9378ccc466131a51b1
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-09-01 13:01:55 +0300

    fixed quizzes error

--- app/dashboard/quizzes/page.tsx
+++ app/dashboard/quizzes/page.tsx
@@ -3,7 +3,7 @@ import { DashboardPageTitle } from "@/components/DashboardPageTitle";
 import { QuizButton } from "@/components/QuizButton";
 import React from "react";
 
-export const quizzes = [
+const quizzes = [
   "Maths Algebra Quiz Grade 1",
   "English Nouns Quiz Grade 2",
   "Kiswahili Vitenzi Quiz Grade4",
