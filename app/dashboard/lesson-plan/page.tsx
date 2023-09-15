f88702461a78f8c72c6a49f5284fdf6e53b22b33
cjephuneh <calebjephuneh@gmail.com>
2023-09-15 08:43:29 +0300

    new_implementation

--- app/dashboard/lesson-plan/page.tsx
+++ app/dashboard/lesson-plan/page.tsx
@@ -1,29 +1,88 @@
 "use client";
 
-import DashboardPageButton from "@/components/DashboardPageButton";
-import { DashboardPageTableHeader } from "@/components/DashboardPageTableHeader";
-import { DashboardPageTitle } from "@/components/DashboardPageTitle";
-import { useRouter } from "next/navigation";
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Page = () => {
+const Dashboard = () => {
   const router = useRouter();
+  const [subject, setSubject] = useState("");
+  const [topic, setTopic] = useState("");
+  const [subStrand, setSubStrand] = useState("");
+  const [grade, setGrade] = useState("");
+  const [duration, setDuration] = useState("");
+
+  
+  const createLessonPlan = async () => {
+    try {
+     
+      const requestBody = {
+        subject: subject,
+        topic: topic,
+        substrand: subStrand,
+        minutes: duration,
+        grade: grade,
+      };
+
+      
+      const response = await axios.post(
+        "https://serverlogic.azurewebsites.net/api/createLessonPlan",
+        requestBody
+      );
+
+      // console.log("Response from server:", response);
+
+      
+      if (response.status === 200) {
+        toast.success("Lesson plan created successfully!");
+        
+       
+        const objectId = response.data._id;
+
+        
+        router.push(`/result?objectId=${objectId}`);
+        
+      }
+    } catch (error) {
+      
+      console.error("Error creating lesson plan:", error);
+      toast.error("Error creating lesson plan. Please try again later.");
+    }
+  };
+
   return (
-    <div className="dashboard-container">
-      <DashboardPageTitle>Lesson Plans</DashboardPageTitle>
-      <DashboardPageButton text="Create New Plan" />
-      <DashboardPageTableHeader />
+    <div className="flex-grow dashboard-container">
+      <div className="bg-white shadow-lg p-[2%] rounded-md">
+        <p className="text-gtahidiDarkBlue font-semibold">
+          Welcome to gTahidi AI
+        </p>
+        <p className="font-semibold">
+          Create Personalized Lesson Plans, Notes And Quizzes With Our Advanced
+          AI
+        </p>
+      </div>
+      <p className="py-2 mt-2 bg-dashboardPurple text-white p-[2%] text-sm rounded-md">
+        Create your well organised lesson plan with just a click of a button.
+        Fill in all the necessary fields according to your preference.
+      </p>
       <div className="w-3/4 sm:w-1/2 mx-auto mt-10 text-sm">
         <div className="flex gap-x-2">
           <input
             type="text"
             placeholder="Enter Subject"
             className="w-1/2 p-3 rounded"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
           />
           <input
             type="text"
             placeholder="Enter Topic"
             className="w-1/2 p-3 rounded"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
           />
         </div>
         <div className="flex gap-x-2 mt-5">
@@ -31,28 +90,36 @@ const Page = () => {
             type="text"
             placeholder="Enter Sub strand"
             className="w-1/2 p-3 rounded"
+            value={subStrand}
+            onChange={(e) => setSubStrand(e.target.value)}
           />
           <input
             type="text"
             placeholder="Enter Grade"
             className="w-1/2 p-3 rounded"
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
           />
         </div>
         <input
           type="text"
           placeholder="Enter Duration in minutes"
           className="w-full p-3 mt-5"
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
         />
+        {/* Button to create the lesson plan */}
         <button
           type="submit"
           className="text-center w-full mt-7 bg-gtahidiPink py-3 text-white rounded-full"
-          onClick={() => router.push("/dashboard/result")}
+          onClick={createLessonPlan}
         >
           Create Lesson Plan
         </button>
       </div>
+      <ToastContainer/>
     </div>
   );
 };
 
-export default Page;
+export default Dashboard;
--- app/dashboard/result/page.tsx
+++ app/dashboard/result/page.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+'use client'
 
-const page = () => {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router"; 
+
+const Result = () => {
+  const [data, setData] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Retrieve the objectId from the query parameters
+    const objectId = router.query.objectId;
+
+    const apiUrl = `https://serverlogic.azurewebsites.net/api/fetchData?objectId=${objectId}`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [router]); 
+
+
   return (
     <div className="flex-grow flex p-5">
-      <p>result</p>
+      <div className="flex-grow overflow-y-auto max-h-screen">
+        {data ? (
+          <div className='max-h-screen'>
+            <h2 className="text-lg font-bold mb-3">{data.subject}</h2>
+            <h3 className="text-md font-semibold mb-2">{data.topic}</h3>
+            <div className="text-gray-700">
+              {data.content.split("\n").map((paragraph, index) => (
+                <p key={index} className="mb-2">
+                  {paragraph}
+                </p>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
       <div className="ml-auto flex flex-col text-white">
         <button className="bg-gtahidiPurple p-3 rounded">Create Notes</button>
         <button className="bg-gtahidiPurple p-3 rounded mt-5">
@@ -14,4 +54,5 @@ const page = () => {
   );
 };
 
-export default page;
+export default Result;
+
