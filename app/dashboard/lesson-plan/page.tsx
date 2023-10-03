908809eae7abb15445edd36cf58f085f2e4d9241
Daniel Omusula <94360277+Cat-Vader@users.noreply.github.com>
2023-10-03 10:30:55 +0300

    Update lesson fetch

--- app/dashboard/lesson-plan/page.tsx
+++ app/dashboard/lesson-plan/page.tsx
@@ -17,6 +17,14 @@ interface LessonPlanData {
   minutes: string;
   content: string;
 }
+interface CustomSession {
+  user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+  };
+}
 
 function formatContent(content: string): string {
   let formattedContent = marked(content); 
@@ -36,20 +44,24 @@ const Page = () => {
     []
   );
   const [loading, setLoading] = useState(true);
+  const { data: session } = useSession() as { data: CustomSession | null };
   const router = useRouter();
 
   useEffect(() => {
     const fetchLessonPlans = async () => {
       try {
-        const response = await fetch(
-          "https://serverlogic.azurewebsites.net/api/fetchLessonPlan?oid=fe2ec27d-8113-4a62-8f0d-d5b7c757b0dd"
-        );
-        if (!response.ok) {
-          throw new Error(
-            "Network response was not ok " + response.statusText
-          );
+        const oid = session?.user?.id // extracting oid
+        if(!oid) {
+          throw new Error("User ID is not found");
         }
-        const data = await response.json();
+          const url = `https://serverlogic.azurewebsites.net/api/fetchLessonPlan/?oid=${oid}`; 
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        
+       const data = await response.json();
         setStoredLessonPlans(data);
       } catch (error) {
         console.error(
@@ -62,7 +74,7 @@ const Page = () => {
     };
 
     fetchLessonPlans();
-  }, []);
+  }, [session]);
 
   const handleBack = () => {
     router.push("/dashboard/createlessonPlan");
