eeb56a0612891d566e3f49f711bd9230f50f8fb7
Daniel Omusula <94360277+Cat-Vader@users.noreply.github.com>
2023-10-03 11:57:33 +0300

    Update page.tsx

--- app/dashboard/createlessonPlan/page.tsx
+++ app/dashboard/createlessonPlan/page.tsx
@@ -105,7 +105,7 @@ const Page = () => {
         <div className="flex gap-x-2">
           <input
             type="text"
-            placeholder="Enter Subject"
+            placeholder="Learning Area"
             className="w-1/2 p-3 rounded"
             name="subject"
             value={formData.subject}
@@ -113,7 +113,7 @@ const Page = () => {
           />
           <input
             type="text"
-            placeholder="Enter Strand"
+            placeholder="Strand"
             className="w-1/2 p-3 rounded"
             name="topic"
             value={formData.topic}
@@ -123,7 +123,7 @@ const Page = () => {
         <div className="flex gap-x-2 mt-5">
           <input
             type="text"
-            placeholder="Enter Sub strand"
+            placeholder="Sub strand"
             className="w-1/2 p-3 rounded"
             name="substrand"
             value={formData.substrand}
@@ -131,7 +131,7 @@ const Page = () => {
           />
           <input
             type="text"
-            placeholder="Enter Grade"
+            placeholder=" Grade"
             className="w-1/2 p-3 rounded"
             name="grade"
             value={formData.grade}
