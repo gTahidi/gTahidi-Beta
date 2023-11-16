08e9b130d0671b0d276a68359909446788420226
cjephuneh <calebjephuneh@gmail.com>
2023-11-16 12:55:40 +0000

    updates

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -9,14 +9,14 @@ export const Welcome = () => {
   return (
     <section className="bg-white p-12 dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
-            <a href="#" className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
-                <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">New</span> <span className="text-sm font-medium">Uploading a lessonPlan was launched! See what's new</span> 
+            <a href="#" className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-gtahidiPink bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
+                <span className="text-xs bg-gtahidiPink rounded-full text-white px-4 py-1.5 me-3">New</span> <span className="text-sm font-medium">Uploading a lessonPlan was launched! See what's new</span> 
                 <svg className="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                 </svg>
             </a>
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Empowering Future <p>Generations </p> </h1>
+            <p className="mb-8 text-sm font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Discover gTahidi AI's innovative learning solutions, all conveniently delivered<br/> through WhatsApp. We bring education to your fingertips ensuring an engaging and specialized learning experience.</p>
             <form className="w-full max-w-md mx-auto">   
                 <label  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email sign-up</label>
                 <div className="relative">
