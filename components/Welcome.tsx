9ef6858a7e952dd5e1fa54a13e4104033c400a2a
cjephuneh <calebjephuneh@gmail.com>
2023-11-20 19:02:16 +0300

    wecome___

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import EducatorsIconSVG from '@/public/educators.svg'; // Update the path to your SVG files
 import LearnersIconSVG from '@/public/earners.svg'; // Update the path to your SVG files
-import EnterprisesIconSVG from '@/public/enterprises-hero.svg'; // Update the path to your SVG files
+import EnterprisesIconSVG from '@/public/enterprise-hero.svg'; // Update the path to your SVG files
 
 
 export const Welcome = () => {
@@ -17,21 +17,63 @@ export const Welcome = () => {
             </a>
             <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Empowering Future <p>Generations </p> </h1>
             <p className="mb-8 text-sm font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Discover gTahidi AI's innovative learning solutions, all conveniently delivered<br/> through WhatsApp. We bring education to your fingertips ensuring an engaging and specialized learning experience.</p>
-            <form className="w-full max-w-md mx-auto">   
-                <label  className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Email sign-up</label>
-                <div className="relative">
-                    <div className="absolute inset-y-0 rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-                            <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
-                            <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
-                        </svg>
-                    </div>
-                    <input type="email" id="default-email" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email here..." required/>
-                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
+            <div className=" flex flex-row space-x-8 items-center justify-center">
+            <div className="w-1/5 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-center pb-5">
+                    <Image 
+                        src={EducatorsIconSVG}
+                        alt="Educators"
+                        width={46}
+                        height={46}
+                    />
                 </div>
-            </form>
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Educators</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400"> This innovative AI service is designed to empower educators and teachers by providing them with cutting-edge AI solutions to streamline the process of creating lesson plans, notes, and quizzes.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more     
+                </a>
+            </div>
+
+            <div className="w-1/5 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-center pb-5">
+                    <Image 
+                        src={LearnersIconSVG}
+                        alt="Educators"
+                        width={46}
+                        height={46}
+                    />
+                </div>
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Learners</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Discover gTahidi AI's innovative learning solutions, all conveniently delivered through WhatsApp. We bring education to your fingertips ensuring an engaging and specialized learning experience.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more     
+                </a>
+            </div>
+            <div className="w-1/5 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex justify-center pb-5">
+                    <Image 
+                        src={EnterprisesIconSVG}
+                        alt="Educators"
+                        width={46}
+                        height={46}
+                    />
+                </div>
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Enterprises</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Unlock the potential of your edtech company with gTahidi AI. Our solutions can empower your platform, enhance the learning experience, and stay ahead of the competition.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more     
+                </a>
+            </div>
+            </div>
+
         </div>
-        <div className="bg-gradient-to-b from-blue-50 to-transparent dark:from-blue-900 w-full h-full absolute top-0 left-0 z-0"></div>
+        
     </section>
 
   );
