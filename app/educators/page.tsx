bdc551c61a9b3692792b5c450a8f4ec7a3227523
cjephuneh <calebjephuneh@gmail.com>
2023-11-23 18:06:03 +0300

    updates

--- app/educators/page.tsx
+++ app/educators/page.tsx
@@ -1,47 +1,17 @@
 // Import React and Next.js components
 import React from 'react';
 import Head from 'next/head';
+import Hero from '@/components/hero_educators';
+import Microsoft from '@/components/microsoft';
 
 // Import Tailwind CSS components
 
 const HomePage = () => {
   return (
-    <>
-      <Head>
-        <title>gTahidi AI</title>
-      </Head>
-      <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center">
-        <div className="text-center p-4 md:p-12">
-          <div className="mb-4">
-            {/* Logo placeholder */}
-            <div className="mb-4 inline-block">
-              {/* Replace with actual logo */}
-              <div className="bg-white p-2 rounded-full">C</div>
-            </div>
-            {/* Title and subtitle */}
-            <h1 className="text-5xl text-white font-bold mb-4">Karibu gTahidi</h1>
-            <p className="text-white text-xl mb-8">
-              Revolutionize your Teaching with gTahidi AI
-            </p>
-            <p className="text-white mb-8">
-              Create Curriculum - Compliant Lesson Plans, Schemes of Work and Assessment Questions in Minutes
-            </p>
-          </div>
-          {/* Button */}
-          <button className="text-purple-600 bg-white hover:bg-gray-100 font-bold py-2 px-4 rounded">
-            Start Learning
-          </button>
-          {/* Stack of Books - This would be an image */}
-          {/* Replace with an actual image */}
-          <div className="mt-8">
-            <img
-              src="/path-to-your-stack-of-books-image.png" // Replace with the path to your image
-              alt="Stack of Books"
-            />
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <Hero />
+      <Microsoft />
+    </div>
   );
 };
 
---
+++ components/hero_educators.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function hero_educators() {
+  return (
+    <div>
+      <div className="container px-6 py-16 mx-auto">
+        <div className="items-center lg:flex">
+            <div className="w-full lg:w-1/2">
+                <div className="lg:max-w-lg">
+                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Best place to choose <br/> your <span className="text-blue-500 ">clothes</span></h1>
+                    
+                    <p className="mt-3 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                    
+                    <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
+                </div>
+            </div>
+
+            <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+                <img className="w-full h-full lg:max-w-3xl" src="https://merakiui.com/images/components/Catalogue-pana.svg" alt="Catalogue-pana.svg"/>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default hero_educators
