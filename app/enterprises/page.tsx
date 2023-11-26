873f8bfbffc5b4e0d77ee33beb8e8ea5c81c3aef
cjephuneh <calebjephuneh@gmail.com>
2023-11-26 22:58:22 +0300

    updates

--- app/enterprises/page.tsx
+++ app/enterprises/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '@/components/enterprise_hero'
 import Microsoft from '@/components/microsoft'
+import Features from '@/components/features_enterprise'
 
 function page() {
   return (
     <div>
       <Hero/>
       <Microsoft/>
+      <Features/>
     </div>
   )
 }
