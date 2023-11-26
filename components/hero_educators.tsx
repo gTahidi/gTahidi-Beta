978bb9f7fe5949dd4c9032653caf757c90b5baa5
cjephuneh <calebjephuneh@gmail.com>
2023-11-26 16:48:52 +0300

    updates

--- components/hero_educators.tsx
+++ components/hero_educators.tsx
@@ -1,89 +1,58 @@
-"use client";
-
-import { useGlobalData } from "@/hooks/useGlobalData";
-import React, { useState } from "react";
-import Typewriter from "typewriter-effect";
+import React from "react";
 import Image from "next/image";
-import Educators from "@/public/educators.svg";
-import Prof from '@/public/prof.png'
+import Educators from "@/public/teacher.png";
+import Prof from '@/public/prof.png';
 
-export const Welcome = () => {
+const Welcome = () => {
   return (
-    <div className=" px-16 py-20 bg-blue-100 ">
-      <section className="container flex flex-col items-center  mx-auto lg:flex-row">
-        <div className="lg:w-1/2">
-          <h1 className="max-w-xl  font-extrabold tracking-wide text-purple-900 capitalize md:text-6xl ">
-            Qaribu <br />
-            gTahidi
+    <div className="bg-blue-100 ">
+      <section className="container mx-auto  py-20 px-20 flex flex-col lg:flex-row items-center justify-between">
+        <div className="flex-1">
+          <h1 className="text-6xl font-extrabold text-purple-900 capitalize">
+            Qaribu <br /> gTahidi
           </h1>
-
-          <p className="max-w-lg mt-4 text-black text-xl font-bold">
+          <p className="mt-4 text-xl font-bold text-black">
             Revolutionize your Teaching with gTahidi
           </p>
-          <p className="max-w-lg mt-4 text-black text-xl font-thin">
-            Create Carriculum - Compliant lessonPlans, Schemes of Wor,
-             And Assesment Questions in Minutes
+          <p className="mt-4 text-xl font-thin text-black">
+            Create Curriculum-Compliant lesson plans, schemes of work, <br/>
+            and assessment questions in minutes.
           </p>
-          <div className="pt-8 lg:pt-10 w-max mx-auto lg:mx-0">
-          <div className="flex items-center text-center flex-col sm:flex-row gap-4 sm:gap-0">
-            <div className="flex items-center">
-              <Image
-                src={Prof}
-                width={40}
-                height={40}
-                alt="avatar"
-                className="w-10 h-10 rounded-full border-2 border-gray-500  object-cover"
-              />
-              <Image
-                src={Prof}
-                width={40}
-                height={40}
-                alt="avatar"
-                className="w-10 h-10 rounded-full border-2 border-gray-500   object-cover -ml-2.5"
-              />
-              <Image
-                src={Prof}
-                width={40}
-                height={40}
-                alt="avatar"
-                className="w-10 h-10 rounded-full border-2 border-gray-500   object-cover -ml-2.5"
-              />
-              <Image
-                src={Prof}
-                width={40}
-                height={40}
-                alt="avatar"
-                className="w-10 h-10 rounded-full border-2 border-gray-500   object-cover -ml-2.5"
-              />
-              <span className="flex items-center justify-center font-medium p-4 text-lg text-gtahidiPink  -ml-2.5">
-                 Join 300+
-              </span>
-            </div>
-            <div className=" text-black">
-              Educators
-            </div>
-          </div>
-        </div>
-        <div />
 
-          <div className="mt-6 sm:flex sm:items-center">
-            <a
-              href="#"
-              className="bg-gtahidiPink  rounded-lg duration-300 transition-colors border-2  px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white "
-            >
-              Start Now
-            </a>
+          <div className="flex items-center space-x-2 mt-8">
+            {[Prof, Prof, Prof, Prof].map((src, index) => (
+              <Image
+                key={index}
+                src={src}
+                width={40}
+                height={40}
+                alt="Educator"
+                className="rounded-full border-2 border-gray-500 object-cover"
+                style={{ marginLeft: index !== 0 ? '-10px' : '0' }}
+              />
+            ))}
+            <span className="text-lg font-medium text-gtahidiPink">
+              Join 300+ Educators
+            </span>
           </div>
+
+          <a
+            href="#"
+            className="mt-6 inline-block bg-gtahidiPink rounded-lg border-2 px-6 py-3 uppercase text-sm font-bold tracking-widest text-white"
+          >
+            Start Now
+          </a>
         </div>
         
-
-        <div className="h-[38rem] mt-12 lg:mt-0 w-full mx-auto max-w-md overflow-hidden">
+        <div className="mt-12 lg:mt-0 w-full max-w-md lg:max-w-lg xl:max-w-xl">
           <Image
-            className="object-cover"
             src={Educators}
-            alt="main page"
-            width={80}
-            height={80}
+            alt="Educators"
+            // Define specific width and height that maintains your aspect ratio
+            width={300} // e.g., if your image's aspect ratio is 2:1, and you want it to be 700px wide
+            height={350} // then the height should be half of the width, which is 350px
+            layout="responsive"
+            objectFit="cover"
           />
         </div>
       </section>
--- components/why_educators.tsx
+++ components/why_educators.tsx
@@ -1,29 +1,72 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const WhyComponent = () => {
   return (
-    // Increased horizontal padding with px-20 and added responsive padding for larger screens with px-32 for larger screens
-    <div className="flex justify-center h-1/2 items-center bg-blue-200 py-14 px-24 lg:px-32">
-      <div className="flex flex-col items-center px-4"> {/* Added padding around each item */}
-        {/* Increased image size */}
-        <Image src="/watch.png" alt="Clock" width={950} height={950} layout="fixed" />
-        <p className="text-center mt-2">Saves you time. (Of Course)</p>
-      </div>
-      <div className="flex flex-col items-center mx-10 px-4"> {/* Increased margin for horizontal spacing and padding around each item */}
-        <Image src="/whiste.png" alt="Whistle" width={950} height={950} layout="fixed" />
-        <p className="text-center mt-2">It's Free. Stop buying Lesson Plans and Schemes of work.</p>
-      </div>
-      <div className="flex flex-col items-center px-4">
-        <Image src="/pen.png" alt="Pencil" width={950} height={950} layout="fixed" />
-        <p className="text-center mt-2">Copy, Edit & Download. It's Yours.</p>
-      </div>
-      <div className="flex flex-col items-center mx-10 px-4">
-        <Image src="/go.png" alt="Rocket" width={950} height={950} layout="fixed" />
-        <p className="text-center mt-2">Compliant with over 80% of Global Curricula.</p>
+    <div className="bg-blue-200 py-14 px-8 lg:px-20">
+      <h2 className="text-4xl text-center font-bold mb-10 text-gray-800">
+        Why gTahidi?
+      </h2>
+      <div className="max-w-8xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="flex flex-col items-center">
+          <div className="relative mb-2">
+            <Image
+              src="/watch.png"
+              alt="Clock"
+              objectFit="contain"
+              height={700}
+              width={700}
+            />
+          </div>
+          <p className="text-center text-sm lg:text-base">
+            Saves you time. (Of Course)
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="  relative mb-2">
+            <Image
+              src="/whiste.png"
+              alt="Whiste"
+              objectFit="contain"
+              height={700}
+              width={700}
+            />
+          </div>
+          <p className="text-center text-sm lg:text-base">
+            It's Free. Stop buying Lesson Plans and Schemes of work.
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="relative mb-2">
+            <Image
+              src="/pen.png"
+              alt="Pencil"
+              objectFit="contain"
+              height={700}
+              width={700}
+            />
+          </div>
+          <p className="text-center text-sm lg:text-base">
+            Copy, Edit & Download. It's Yours.
+          </p>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className=" relative mb-2">
+            <Image
+              src="/go.png"
+              alt="Rocket"
+              objectFit="contain"
+              height={700}
+              width={700}
+            />
+          </div>
+          <p className="text-center text-sm lg:text-base">
+            Compliant with over 80% of Global Curricula.
+          </p>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default WhyComponent;
