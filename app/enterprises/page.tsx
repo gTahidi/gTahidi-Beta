4823c2b6fbebdf97488f41090715b75724c08c02
cjephuneh <calebjephuneh@gmail.com>
2023-11-28 15:56:21 +0300

    updates

--- app/enterprises/page.tsx
+++ app/enterprises/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Hero from '@/components/hero_learners'
+import Microsoft from '@/components/microsoft'
+
 
 function page() {
   return (
+    <>
     <div className=' py-40 h-full'>
         <Hero/>
     </div>
+    <Microsoft/>
+    </>
+
   )
 }
 
