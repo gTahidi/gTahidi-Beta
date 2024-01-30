19beb7893ed4d42ed80f63811c0d319d60c30235
cjephuneh <calebjephuneh@gmail.com>
2024-01-30 14:29:02 +0300

    updates

--- components/learn.tsx
+++ components/learn.tsx
@@ -12,7 +12,7 @@ function learn() {
                 <h1 className="font-bold text-white text-3xl md:text-4xl mb-6">Join us in our mission to revolutionize education. gTahidi AI is not just a tool; it's a partner in creating a future where learning is limitless</h1>
                 
                 <div className="mt-5 lg:mt-8 flex justify-center lg:justify-start flex-col items-center gap-2 sm:flex-row sm:gap-3">
-                    <a className="w-full sm:w-auto md:py-10 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gtahidiPink text-white disabled:opacity-50 disabled:pointer-events-none " href="#">
+                    <a className="w-full sm:w-auto p-3 inline-flex justify-center items-center  text-sm font-semibold rounded-lg border border-transparent bg-gtahidiPink text-white  " href="#">
                     Start Learning
                     </a>
                 </div>
