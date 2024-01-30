32b4d338418fe4dde09886af5f72bffe1cb82934
cjephuneh <calebjephuneh@gmail.com>
2024-01-30 14:47:36 +0300

    updates

--- components/hero_educators.tsx
+++ components/hero_educators.tsx
@@ -8,7 +8,7 @@ const Welcome = () => {
     <div className="bg-blue-100 mt-16 md:mt-0">
       <section className="container mx-auto py-10 px-4 md:py-20 md:px-20 flex flex-col lg:flex-row items-center justify-between">
         <div className="flex-1 text-center lg:text-left">
-          <h1 className="text-8xl md:font-extrabold font-extrabold text-purple-900 capitalize " >
+          <h1 className="text-8xl md:font-extrabold font-extrabold text-purple-900  " >
             karibu <br/> gTahidi
           </h1>
           <p className="mt-4 text-lg md:text-xl font-bold text-black">
